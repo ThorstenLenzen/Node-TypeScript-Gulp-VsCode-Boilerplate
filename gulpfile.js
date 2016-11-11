669083725fb9bdf038903ce99b8c95790999849c
ThorstenLenzen <thorstenlenzen@outlook.com>
2016-11-11 11:34:36 +0100

    Typings were removed

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,9 @@
 var gulp = require('gulp');
 var typescript = require('gulp-typescript');
 var sourcemaps = require('gulp-sourcemaps');
-var sync = require('gulp-sync')(gulp);
 var tslint = require('gulp-tslint');
 var del = require('del');
 var path = require('path');
-
 
 gulp.task('clean-app', function () {
     return del([
@@ -28,21 +26,6 @@ gulp.task('ts-transpile', function() {
         .pipe(sourcemaps.write('.', {destPath: './app'}))
         .pipe(gulp.dest('./app'));
 });
-
-// gulp.task('ts-transpile', function() {
-//     var tsProject = typescript
-//         .createProject('tsconfig.json');
-
-//     var tsResult = tsProject
-//         .src('./app/**/*.ts')
-//         .pipe(sourcemaps.init())
-//         .pipe(tsProject());
- 
-//     return tsResult
-//         .js
-//         .pipe(sourcemaps.write('.'))
-//         .pipe(gulp.dest('./app'));
-// });
 
 gulp.task('ts-watch', ['ts-transpile'], function() {
     gulp.watch('./app/**/*.ts', ['ts-transpile']);
